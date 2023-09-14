6aeb7701fc73aedaa096cd8741ff06063b2357d8
dr-joe-wirth <dr.joe.wirth@gmail.com>
2023-09-14 14:34:21 -0400

    fixed clean up

--- espwAlleleCaller.py
+++ espwAlleleCaller.py
@@ -4,7 +4,35 @@
 from auxillary.Parameters import Parameters
 from auxillary.blastn import _blastn
 from auxillary.ariba import _ariba
-import os
+import os, shutil
+
+
+def __cleanup(params:Parameters) -> None:
+    """delete any files created by this program using values specified in a Parameters object
+
+    Args:
+        params (Parameters): a Parameters object
+    """
+    # remove blastdb
+    if params._blastDb is not None:
+        if os.path.exists(os.path.dirname(params._blastDb)):
+            shutil.rmtree(os.path.dirname(params._blastDb))
+    
+    # remove blastn result
+    if params._blastFn is not None:
+        if os.path.exists(params._blastFn):
+            os.remove(params._blastFn)
+    
+    # remove ariba results
+    if params._aribaDir is not None:
+        if os.path.exists(params._aribaDir):
+            shutil.rmtree(params._aribaDir)
+    
+    # remove ariba db
+    if params._aribaDb is not None:
+        if os.path.exists(params._aribaDb):
+            shutil.rmtree(params._aribaDb)
+
 
 def __main() -> None:
     """main runner function
@@ -29,9 +57,7 @@ def __main() -> None:
 
         # delete files if requested
         if params.cleanup:
-            os.rmdir(os.path.dirname(params._blastdb))
-            os.remove(params._blastFn)
-            os.rmdir(params._aribaDir)
+            __cleanup(params)
         
         print(allele)
 
