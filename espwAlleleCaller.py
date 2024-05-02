424aaf037cf175db2d18d17578b38e78c30a7a6b
dr-joe-wirth <dr.joe.wirth@gmail.com>
2024-05-02 13:29:22 -0400

    updated version

--- espwAlleleCaller.py
+++ espwAlleleCaller.py
@@ -11,7 +11,7 @@ from downloadAssemblies import _runner as _downloadAssemblies
 
 
 __author__ = "Joseph S. Wirth"
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 
 def _parseFile(fn:str) -> dict[str,tuple[str,str]]:
